46d7e1c68f510e1a2b904a379b22506b2ef2aa66
jahids <evanjahid321@gmail.com>
2022-02-09 20:35:45 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const bodyparser = require('body-parser');
+const path = require('path');
+
+
+const app = express();
+
+dotenv.config({path:'config.env'})
+const PORT = process.env.PORT || 8000
+
+// log request
+app.use(morgan('tiny'));
+
+// parse request to body-parser
+app.use(bodyparser.urlencoded({extended:true}))
+
+// set view engine
+
+app.set("view engine" , "ejs")
+// app.set('views', path.resolve(__dirname, "views/ejs"))
+
+
+// load assets connect js img and css
+app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
+app.use('/css', express.static(path.resolve(__dirname, "assets/js")))
+app.use('/css', express.static(path.resolve(__dirname, "assets/img")))
+
+
+
+
+app.get('/', (req,res)=> {
+    res.send('Crud Application');
+})
+
+app.listen(PORT,()=>{
+    console.log(`server is runnig on http://localhost:${PORT}`)})
